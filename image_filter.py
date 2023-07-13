ea924d99bd0351ad33602bbdec126f29dbfd2c27
Honey Sharma <honey.y.sharma@gmail.com>
2023-07-13 19:46:25 +1000

    Updated image filters to keep the original order of the frames, files get sorted out by extensions in alphabetical order.

--- image_filter.py
+++ image_filter.py
@@ -19,13 +19,21 @@ def filter_extensions_regex(input_files=None, image_extensions=None):
         return
         
     extensions = "|".join(image_extensions)
-    reg_exp = re.compile(r'.*\.({e})$'.format(e=extensions))
+    # reg_exp = re.compile(r'.*\.({e})$'.format(e=extensions))
 
     # alternative regex
-    # reg_exp = re.compile(r'.*[0-9]\.({e})$'.format(e=extensions))
+    reg_exp = re.compile(r'.*[0-9]\.({e})$'.format(e=extensions))
 
     filtered_files = [f for f in input_files if re.match(reg_exp, f)]
-    output_files = sorted(filtered_files, key=lambda f: os.path.splitext(f)[::-1])
+
+    # to sort the frames use this
+    # output_files = sorted(filtered_files, key=lambda f: os.path.splitext(f)[::-1])
+
+    # to maintain the original order of frames use this
+    output_files = sorted(filtered_files, key=lambda f: os.path.splitext(f)[1])
+
+    # to keep input order
+    # output_files = filtered_files
 
     print("Filter with regex".center(100, '-'))
     for f in output_files:
@@ -49,7 +57,15 @@ def filter_extensions_any(input_files=None, image_extensions=None):
         return
 
     filtered_files = [f for f in input_files if any(f.endswith('.'+e) for e in image_extensions)]
-    output_files = sorted(filtered_files, key=lambda f: os.path.splitext(f)[::-1])
+
+    # to sort the frames use this
+    # output_files = sorted(filtered_files, key=lambda f: os.path.splitext(f)[::-1])
+
+    # to maintain the original order of frames use this
+    output_files = sorted(filtered_files, key=lambda f: os.path.splitext(f)[1])
+
+    # to keep input order
+    # output_files = filtered_files
 
     print("Filter with any()".center(100, '-'))
     for f in output_files:
@@ -81,7 +97,15 @@ def filter_extensions_endswith(input_files=None, image_extensions=None):
 
     image_extensions = tuple(['.'+e for e in image_extensions])
     filtered_files = [f for f in input_files if f.endswith(image_extensions)]
-    output_files = sorted(filtered_files, key=lambda f: os.path.splitext(f)[::-1])
+
+    # to sort the frames use this
+    # output_files = sorted(filtered_files, key=lambda f: os.path.splitext(f)[::-1])
+
+    # to maintain the original order of frames use this
+    output_files = sorted(filtered_files, key=lambda f: os.path.splitext(f)[1])
+
+    # to keep input order
+    # output_files = filtered_files
 
     print("Filter with endswith tuple".center(100, '-'))
     for f in output_files:
@@ -90,9 +114,24 @@ def filter_extensions_endswith(input_files=None, image_extensions=None):
 
 if __name__ == "__main__":
 
-    path = r'C:\Users\honey\Pictures\images'
-    files = [os.path.join(path, f) for f in os.listdir(path) if not os.path.isdir(f)]
-    extensions = ["jpg", "png"]
-    # filter_extensions_regex(files, extensions)
-    # filter_extensions_any(files, extensions)
+    files = [
+    r"C:\Users\honey\Pictures\images\flower1001.jpg",
+    r"C:\Users\honey\Pictures\images\flower1000.jpg",
+    r"C:\Users\honey\Pictures\images\flower1002.jpg",
+    r"C:\Users\honey\Pictures\images\test.0000.jpg",
+    r"C:\Users\honey\Pictures\images\image.2000.exr",
+    r"C:\Users\honey\Pictures\images\image.1001.exr",
+    r"C:\Users\honey\Pictures\images\image.2002.exr",
+    r"C:\Users\honey\Pictures\images\mushroom.1111.png",
+    r"C:\Users\honey\Pictures\images\test.2111.tiff",
+    r"C:\Users\honey\Pictures\images\test.1110.tiff"
+    ]
+    print("Input".center(100, '-'))
+    for f in files:
+        print('{0}'.format(f))
+
+    extensions = ["jpg", "exr", "tiff"]
+
+    filter_extensions_regex(files, extensions)
+    filter_extensions_any(files, extensions)
     filter_extensions_endswith(files, extensions)

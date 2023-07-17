177a607b1314a7e23ff8fd0b081492ff670771e1
Honey Sharma <honey.y.sharma@gmail.com>
2023-07-17 23:36:15 +1000

    Cleaned up code.

--- unity_tests/rename_image_seq.py
+++ unity_tests/rename_image_seq.py
@@ -8,27 +8,27 @@ def solution(A):
     :return: a list of renamed image sequences, with original order
     """
 
-    # This pattern r"\.\d+\.\w{2,}$" matches a dot (\.) followed by one or more digits, followed by another dot (\.),
-    # followed by 1 or more word characters (\w) until the end of the string ($).
-    regex = re.compile(r"\.\d+\.\w{1,}$")
+    # The pattern r"\.\d+\.\w+$" matches a dot (\.) followed by one or more digits, followed by another dot (\.),
+    # followed by one or more word characters (\w) until the end of the string ($).
+    regex = re.compile(r"\.\d+\.\w+$")
 
     # a dictionary to save image counter for each "name.ext" key, example: {"foo.jpg": 3, "bar.png" 1}
     image_map = dict()
 
-    # initialize a list, same length as input.
+    # initialize the output list with the same length as input.
     output = [None] * len(A)
 
-    # create index dictionary to get index in the loop, efficient than doing A.index(image)
+    # create index dictionary to get index in the loop below, efficient than doing A.index(image)
     index_map = {image: index for index, image in enumerate(A)}
 
-    # sort the input list to get the correct padding and maintain the order, that is [2, 4, 3], 4 gets higher padding
-    # than 3
+    # sort the input list to get the correct padding and maintain the order, that is, in the list [2, 4, 3],
+    # 4 gets higher padding than 3
     sorted_files = sorted(A)
     for image in sorted_files:
-        # get the index of the image from the original list
+        # get the index of the image from the index_map
         index = index_map[image]
 
-        # check if the image matches the <name>.<padding>.<extension> pattern, valid extension is longer than 2 letters.
+        # check if the image matches the <name>.<padding>.<extension> pattern.
         if re.search(regex, image):
             # split image name in name, number and extension
             name, num, ext = image.split('.')
@@ -36,15 +36,15 @@ def solution(A):
             # create a key with name.extension (foo.jpg)
             key = name + '.' + ext
 
-            # check if the key exists in the image_map, if it doesn't, initialize counter by 0 then add 1 to it
+            # check if the key exists in the image_map, if it doesn't, initialize counter by 0, then increment by 1
             # {foo.jpg: 2, bar.png: 1}
             image_map[key] = image_map.get(key, 0) + 1
 
-            # get the counter from the image map and create new name with the padding of 4 (foo.0002.jpg)
+            # get the counter from the image map and create the new name with the padding of 4 digits (foo.0002.jpg)
             image = name + '.' + str(image_map[key]).zfill(4) + '.' + ext
 
         # if the image name doesn't match the pattern, add it to the original index unaltered.
-        # if the image name matches the pattern, add it to the original index.
+        # if the image name matches the pattern, add the updated image name to the original index.
         output[index] = image
     return output
 
